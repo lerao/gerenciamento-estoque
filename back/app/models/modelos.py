2a55612789ddf5c99733b52490c5440e673a9dee
MarcosViniDLL <marcosviniciuscontatooficial@gmail.com>
2023-11-15 23:06:16 -0300

    modelos_py rotas_py

--- back/app/models/modelos.py
+++ back/app/models/modelos.py
@@ -1,20 +1,41 @@
-from app import db
+###from app import db
 
 # ####################
 # Cada classe é uma tabela no banco de dados
 # ####################
 
 #Casa herda todas as características de db.Model
-class Casa(db.Model):
+#class Casa(db.Model):
 
     #Propriedades de Casa
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    qtdQuartos = db.Column(db.Integer)
-    qtdBanheiros = db.Column(db.Integer)
-    rua = db.Column(db.String(100))
+   # id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  #  qtdQuartos = db.Column(db.Integer)
+   # qtdBanheiros = db.Column(db.Integer)
+   # rua = db.Column(db.String(100))
 
     #Construtor, recebendo os parâmetros para adicionar ao objeto
-    def __init__(self, qtdQuartos, qtdBanheiros, rua):
-        self.qtdQuartos = qtdQuartos
-        self.qtdBanheiros = qtdBanheiros
-        self.rua = rua
+   # def __init__(self, qtdQuartos, qtdBanheiros, rua):
+   #     self.qtdQuartos = qtdQuartos
+    #    self.qtdBanheiros = qtdBanheiros
+    #    self.rua = rua###
+
+##########################################################################################################################################
+from app import db
+
+class cliente(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(80))
+    tipo = db.Colmn(db.String(20))
+    documento = db.Column(db.String(19))
+    endereco = db.Column(db.String(100))
+    telefone = db.Column(db.String(20))
+    datanascimento = db.Column(db.Date)
+
+    def __init__(self, nome, tipo, documento, endereco, telefone, datanascimento):
+        self.nome = nome
+        self.tipo = tipo
+        self.documento = documento
+        self.endereco = endereco
+        self.telefone = telefone
+        self.datanascimento = datanascimento
