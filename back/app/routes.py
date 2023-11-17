07ae0fc521182039cb74834552d6510c41a6b9c7
devmarcs <devmarcelo.gus@gmail.com>
2023-11-16 22:26:41 -0300

    Inserção da tabela tarefa e das rotas GET POST

--- back/app/routes.py
+++ back/app/routes.py
@@ -4,11 +4,7 @@ from sqlalchemy.exc import IntegrityError
 from datetime import datetime
 
 #Importa os modelos que vamos usar
-<<<<<<< HEAD
-from app.models.modelos import Casa, Tarefa
-=======
-from app.models.modelos import Casa, Cliente
->>>>>>> 8ae4ddfd448e3cf257e3d907360b91c714dea358
+from app.models.modelos import Casa, Cliente, Tarefa
 
 #CRUD
 # Create -> Cria recurso -> POST
@@ -130,7 +126,38 @@ def delete_cliente(cliente_id):
 
 
 
+#===== Listar Tarefas =====
+@app.route("/tarefa", methods=['GET'])
+def lista_tarefas():
+    tarefas = Tarefa.query.all()
+    lista_tarefa = []
 
+    for tarefa in tarefas:
+        lista_tarefa.append({
+            'id': tarefa.id,
+            'descricao': tarefa.descricao,
+            'status': tarefa.status
+        })
+
+    return jsonify({'tarefas': lista_tarefa})
+
+
+#===== Criar Tarefas =====
+@app.route("/tarefa", methods=['POST'])
+def criar_tarefa():
+
+    dados = request.json
+
+    if 'descricao' not in dados:
+        return jsonify({'status': 400, 'message': 'Descrição da tarefa é obrigatória'}), 400
+
+    descricao = dados['descricao']
+    nova_tarefa = Tarefa(descricao=descricao)
+
+    db.session.add(nova_tarefa)
+    db.session.commit()
+
+    return jsonify({'status': 201, 'message': 'Tarefa criada com sucesso'}), 201
 """
 
 # Excluir uma categoria
